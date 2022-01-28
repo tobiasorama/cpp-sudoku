0e8493d90e50b2158b85264c03aaaff08492c78b
Tobias Oliver Eklund <31617845+tobiasorama@users.noreply.github.com>
2022-01-28 15:51:49 +0100

    Update board.h
    Cleaner code, as well as adding the possibility of n x m boards.

--- board.h
+++ board.h
@@ -1,169 +1,102 @@
+#include <vector>
+#include <sstream>
+#include <iomanip>
+
 class Board {
-  int board [3][3][3][3];
-  bool has_value [3][3][3][3];
+  int rows, cols, size;
+  std::vector<int> board;
+  std::vector<bool> has_value;
   bool _solved = false;
-  /*
-    the two first indices give the position of a 3x3 box
-    the last two indices give the position of values inside a box
-  */
 
-  /*
-  1 2 3  4 5 6  7 8 9
-  4 5 6  7 8 9  1 2 3
-  7 8 9  1 2 3  4 5 6
-
-  2 1 4  3 6 5  8 9 7
-  3 6 5  8 9 7  2 1 4
-  8 9 7  2 4 1  6 3 5
-
-  5 7 2  9 1 4  3 6 8
-  9 4 1  6 3 8  5 7 2
-  6 3 8  5 7 2  9 4 1
-  */
-
+  bool solver(int pos) {
+    if (pos >= size*size)
+      return true;
+    if (board[pos] == 0) {
+      for (int m = 1; m <= size; m++)
+        if (add_value(pos, m)) {
+          if (solver(pos+1)) {
+            return true;
+          } else {
+            board[pos] = 0;
+          }
+        }
+      return false;
+    }
+    return solver(pos+1);
+  }
+  
   public:
-  Board() {
-    //dummy board for debugging purposes
-    for (int i = 0; i < 3; i++)
-      for (int j = 0; j < 3; j++)
-        for (int k = 0; k < 3; k++)
-          for (int l = 0; l < 3; l++)
-            board[i][j][k][l] = 0;
-  }
-
-  Board(std::string s) {
-    int row, column;
-    int index = -2;
-
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-        if (j < 3) {
-          column = 0;
-        } else if (j < 6) {
-          column = 1;
-        } else {
-          column = 2;
-        }
-
-        if (i < 3) {
-          row = 0;
-        } else if (i < 6) {
-          row = 1;
-        } else {
-          row = 2;
-        }
-        index += 2 + (j % 3 == 0 && j > 0? 1: 0) + (i % 3 == 0 && i > 0 && j == 0);
-        has_value[row][column][i % 3][j % 3] = s.at(index) == '0'? false: true;
-        board[row][column][i % 3][j % 3] = s.at(index) - 48;
+  Board(std::ifstream &f) {
+    f >> rows >> cols;
+    size = rows*cols;
+    int i = 0;
+    board.resize(size*size);
+    has_value.resize(size*size);
+    while (f.good()) {
+      f >> board[i];
+      has_value[i] = (bool)board[i];
+      i++;
       }
-    }
   }
 
-  void add_value(int i, int j, int k, int l, int add) {
-    if (has_value[i][j][k][l]) {
-      std::cout << "This position has predetermined value\n\n";
-    } else if (!legal_addition(i, j, k, l, add)) {
-      std::cout << "Illegal addition\n\n";
-    } else {
-      board[i][j][k][l] = add;
-    }
+  bool add_value(int row, int col, int add) {
+    return add_value(row *size + col, add);
   }
 
-  bool legal_addition(int i, int j, int k, int l, int add) {
+  bool add_value(int pos, int add) {
+    if (!add)
+      return true;
+    int col = pos % size;
+    int row = (pos - col)/size;
     //check box
-    for (int s = 0; s < 3; s++)
-      for (int t = 0; t < 3; t++)
-        if ((s != k || t != l) && board[i][j][s][t] == add)
+    int start_row = row - (row % rows);
+    int start_col = col - (col % cols);
+    for (int i = 0; i < rows; i++)
+      for (int j = 0; j < cols; j++)
+        if (
+            (row != start_row + i || col != start_col + j) &&
+            add == board[(start_row + i)*size + (start_col + j)]
+        ) {
           return false;
+        }
 
-    //check row and column
-    for (int s = 0; s < 3; s++)
-      for (int t = 0; t < 3; t++) {
-        if (s != i && board[s][j][t][l] == add)
-          return false;
-        if (s != j && board[i][s][k][t] == add)
-          return false;
-      }
-
+    //check column and row
+    for (int i = 0; i < size; i++)
+      if (i != row && board[i*size + col] == add)
+        return false;
+    for (int j = 0; j < size; j++)
+      if (j != col && board[row*size + j] == add)
+        return false;
+    board[pos] = add;
     return true;
   }
 
-  private:
-  bool solver(int i, int j, int k, int l) {
-    //calculate next position
-    int ni = i;
-    int nj = j;
-    int nk = k;
-    int nl = l;
-    if (j == 2) {
-      if (l == 2) {
-        nj = 0;
-        nl = 0;
-        if (k == 2) {
-          nk = 0;
-          ni += 1;
-        } else {
-          nk += 1;
-        }
-      } else {
-        nl += 1;
-      }
-    } else {
-      if (l == 2) {
-        nl = 0;
-        nj += 1;
-      } else {
-        nl += 1;
-      }
-    }
-
-    if (board[i][j][k][l] == 0) {
-      for (int m = 1; m <= 9; m++) {
-        if (legal_addition(i, j, k, l, m)) {
-          board[i][j][k][l] = m;
-          if ((i == 2 && j == 2 && k == 2 && l == 2) || solver(ni, nj, nk, nl)) {
-            return true;
-          } else {
-            board[i][j][k][l] = 0;
-          }
-        }
-      }
-
-      return false;
-    }
-
-    if (legal_addition(i, j, k, l, board[i][j][k][l])) {
-      if (i == 2 && j == 2 && k == 2 && l == 2)
-        return true;
-      return solver(ni, nj, nk, nl);
-    }
-
-    return false;
-  }
-
-  public:
   bool solved() {
     return _solved;
   }
 
   void solve() {
-    _solved = solver(0, 0, 0, 0);
+    _solved = solver(0);
   }
 
   std::string make_string() {
-    std::string s = "";
-    for (int i = 0; i < 3; i++) {
-      for (int k = 0; k < 3; k++) {
-        for (int j = 0; j < 3; j++) {
-          for (int l = 0; l < 3; l++) {
-            s += (has_value[i][j][k][l]? "\033[96m": "") + std::to_string(board[i][j][k][l]) + "\33[0m "; //using ansi escape sequences to colour in predetermined values
+    int width = log10(size)  + 1;
+    std::ostringstream s;
+    int pos = 0;
+    for (int i = 0; i < rows; i++) {
+      for (int k = 0; k < rows; k++) {
+        for (int j = 0; j < cols; j++) {
+          for (int l = 0; l < cols; l++) {
+            //using ansi escape sequences to colour in predetermined values
+            s << (has_value[pos]? "\033[96m": "") << std::setw(width) << std::to_string(board[pos]) << "\33[0m ";
+            pos += 1;
           }
-          s += " ";
+          s << " ";
         }
-        s += "\n";
+        s << "\n";
       }
-      s += i < 2? "\n": "";
+      s << (i < rows? "\n": "");
     }
-    return s;
+    return s.str();
   }
 };
